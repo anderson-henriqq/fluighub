a57f7b9808f2eb424af290ad194e36969c95c9ce
Anderson Henrique <anderson.henriq10@gmail.com>
2025-04-16 14:50:46 -0300

    teste

--- teste.java
+++ teste.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Versão 2 now");
+        System.out.println("Versão 2");
+        System.out.println("Versão 2");
     }
 }
