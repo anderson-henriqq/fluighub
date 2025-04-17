495a32af4d3c3ea1a4b03d9ea81b4685058a340c
Anderson Henrique <anderson.henriq10@gmail.com>
2025-04-17 14:18:23 -0300

    Teste apos mudar workflow

--- teste.java
+++ teste.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Estou estacando a Versão 2");
+        System.out.println("Estou estacando a Versão 2 - teste");
     }
 }
