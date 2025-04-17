c40d139ba9bb1da78650788d6059baa432eef79a
Anderson Henrique <anderson.henriq10@gmail.com>
2025-04-17 15:50:27 -0300

    Alteração de nome base da branchs

--- create_cliente.sh
+++ create_cliente.sh
@@ -1,8 +1,11 @@
-CLIENTE=fluigsebraern
+CLIENTE=sebraern
+BASE=fluighub
+CLIENTE=$BASE$CLIENTE
 RELEASE=main
 
+
 # Cria branch do cliente baseada em versão
-git checkout $CLIENTE $RELEASE
+git checkout $CLIENTE 
 
 # Cria branches de ambiente
 git checkout $CLIENTE-prod
